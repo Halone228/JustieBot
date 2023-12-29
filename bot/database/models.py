244e224b0e2dd1191f4dffea42aac0cd8c51e0c2
halone <haloneprog@gmail.com>
2023-12-29 14:02:57 +0300

    Update models

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,27 +1,23 @@
 from .core import Base
 from datetime import datetime
-from sqlalchemy import func, BigInteger
+from sqlalchemy import func, BigInteger, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(Base):
-    __tablename__ = "user_table"
+    __tablename__ = "user_points_table"
     user_id: Mapped[int] = mapped_column(
-        BigInteger,
+        ForeignKey('users_table.id'),
         primary_key=True
-    )
-    expire_date: Mapped[datetime] = mapped_column(
-        server_default=func.now()
-    )
-    notified: Mapped[bool] = mapped_column(
-        default=True
-    )
-    in_group: Mapped[bool] = mapped_column(
-        default=False
     )
     points: Mapped[float] = mapped_column(
         default=0
     )
-    messages: Mapped[int] = mapped_column(
-        default=0
+
+
+class Users(Base):
+    __tablename__ = "users_table"
+    id: Mapped[int] = mapped_column(
+        BigInteger(),
+        primary_key=True
     )
